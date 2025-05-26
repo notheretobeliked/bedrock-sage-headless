59f568c3ca3306336237339447146898fdbbb085
Erik Hartin <erik@nhtbl.studio>
2025-05-26 23:12:23 +0100

    Feat: Enable previews with token

--- web/app/themes/sage-headless/functions.php
+++ web/app/themes/sage-headless/functions.php
@@ -54,7 +54,7 @@ if (! function_exists('\Roots\bootloader')) {
 |
 */
 
-collect(['setup', 'filters'])
+collect(['preview-integration', 'setup', 'filters'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
